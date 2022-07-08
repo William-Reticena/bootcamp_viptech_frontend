0cd2d23ab346db0f583799d5f76ae671c73f64e8
William Reticena <william.wsd43@gmail.com>
2022-07-08 13:43:18 -0300

    :sparkles: terminada a tela de adicionar progutos

--- src/pages/AddProduct/index.js
+++ src/pages/AddProduct/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import { AddPhotoAlternate } from "@mui/icons-material";
 import {
@@ -15,11 +15,15 @@ import {
   ThemeProvider,
   Typography,
 } from "@mui/material";
+import { DesktopDatePicker } from "@mui/x-date-pickers";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { Header, Layout } from "../../components";
 import { styles, theme } from "./styles";
 import { LIST_PRODUCTS } from "../../routes/routes";
 
 export const AddProduct = () => {
+  const [value, setValue] = useState(new Date());
   const navigate = useNavigate();
 
   const breadcrumbs = [
@@ -29,18 +33,23 @@ export const AddProduct = () => {
       color="inherit"
       onClick={() => navigate(LIST_PRODUCTS)}
     >
-      MUI
+      Home
     </Link>,
-    <Typography key="3" color="text.primary">
+    <Typography key="2" color="text.primary">
       Adicionar Produto
     </Typography>,
   ];
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <Layout>
       <Header />
 
       <ThemeProvider theme={theme}>
-        <Box component="form" sx={styles.boxMain}>
+        <Box sx={styles.boxMain}>
           <Breadcrumbs separator=">" sx={styles.breadcrumbs}>
             {breadcrumbs}
           </Breadcrumbs>
@@ -49,62 +58,81 @@ export const AddProduct = () => {
             Adicionar Produto
           </Typography>
 
-          <TextField
-            fullWidth
-            label="Nome do Produto"
-            margin="normal"
-            size="small"
-            placeholder="Digite o nome do produto"
-          />
-
-          <TextField
-            fullWidth
-            label="Marca"
-            margin="normal"
-            placeholder="Digite a marca do produto"
-            size="small"
-          />
-
-          <Box sx={styles.boxSub}>
+          <Box component="form">
             <TextField
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">R$</InputAdornment>
-                ),
-              }}
-              label="Valor"
+              fullWidth
+              label="Nome do Produto"
               margin="normal"
-              placeholder="000,00"
+              size="small"
+              placeholder="Digite o nome do produto"
+            />
+
+            <TextField
+              fullWidth
+              label="Marca"
+              margin="normal"
+              placeholder="Digite a marca do produto"
               size="small"
             />
 
-            <FormControl fullWidth sx={styles.formControl} size="small">
-              <InputLabel id="select-color">Cor</InputLabel>
-              <Select
-                id="select-color"
-                label="Cor"
-                name="productColor"
-                value=""
-                // onChange={formik.handleChange}
-              >
-                <MenuItem value={"Branco"}>Branco</MenuItem>
-                <MenuItem value={"Preto"}>Preto</MenuItem>
-                <MenuItem value={"Azul"}>Azul</MenuItem>
-              </Select>
-            </FormControl>
+            <Box sx={styles.boxSub}>
+              <TextField
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">R$</InputAdornment>
+                  ),
+                }}
+                label="Valor"
+                margin="normal"
+                placeholder="000,00"
+                size="small"
+              />
 
-            <TextField
-              label="Data de Cadastro"
-              margin="normal"
-              size="small"
-              // type="date"
-            />
-            <AddPhotoAlternate />
+              <FormControl fullWidth sx={styles.formControl} size="small">
+                <InputLabel id="select-color">Cor</InputLabel>
+                <Select
+                  id="select-color"
+                  label="Cor"
+                  name="productColor"
+                  value=""
+                  // onChange={formik.handleChange}
+                >
+                  <MenuItem value={"Branco"}>Branco</MenuItem>
+                  <MenuItem value={"Preto"}>Preto</MenuItem>
+                  <MenuItem value={"Azul"}>Azul</MenuItem>
+                </Select>
+              </FormControl>
 
-            <Box>
-              <Button fullWidth variant="contained" size="large">
-                Adicionar Produto
-              </Button>
+              <FormControl fullWidth sx={styles.formControl}>
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                  <DesktopDatePicker
+                    label="Data de Cadastro"
+                    inputFormat="dd/MM/yyyy"
+                    value={value}
+                    onChange={handleChange}
+                    renderInput={(params) => (
+                      <TextField {...params} size="small" />
+                    )}
+                  />
+                </LocalizationProvider>
+              </FormControl>
+
+              <Box sx={styles.boxIcon}>
+                <AddPhotoAlternate color="grey" sx={styles.icon} />
+                <Typography
+                  color="gray"
+                  sx={styles.typoCaption}
+                  variant="caption"
+                >
+                  Adicionar fotos
+                </Typography>
+              </Box>
+
+              <Box>
+                <Button fullWidth variant="contained" size="large">
+                  Adicionar Produto
+                </Button>
+              </Box>
             </Box>
           </Box>
         </Box>
--- src/pages/AddProduct/styles.js
+++ src/pages/AddProduct/styles.js
@@ -5,6 +5,9 @@ export const theme = createTheme({
     primary: {
       main: "#00264B",
     },
+    grey: {
+      main: "#D9D9D9",
+    },
   },
 });
 
@@ -28,5 +31,24 @@ export const styles = {
 
   formControl: {
     margin: "16px 0 8px",
+  },
+
+  boxIcon: {
+    alignItems: "center",
+    border: "1px dashed #D9D9D9",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    margin: "8px 0 24px",
+    padding: "24px 8px 0 8px",
+    width: "88px",
+  },
+
+  icon: {
+    fontSize: "40px",
+  },
+
+  typoCaption: {
+    textAlign: "center",
   },
 };
